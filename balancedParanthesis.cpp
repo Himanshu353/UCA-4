c6542a0ce01067a5c14bc3225b693baf8ec6036b
Himanshu Kakkar <42165499+Himanshu353@users.noreply.github.com>
2019-08-17 12:24:28 +0530

    Add files via upload

---
+++ balancedParanthesis.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    stack<char> s;
+    string str;
+    cin>>str;
+    int i,f=0;
+    for(i=0;str[i];i++)
+    {
+        if(str[i]=='('||str[i]=='{'||str[i]=='[')
+        {
+        	s.push(str[i]);
+		}
+        else if(s.empty())
+        {
+            f=1;
+            break;
+        }
+        else if((str[i]==')' && s.top()=='(')||(str[i]=='}' && s.top()=='{')||(str[i]==']' && s.top()=='['))
+        {
+            s.pop();
+        }
+        else
+        {
+            {
+                f=1;
+                break;
+            }
+        }
+    }
+    if(f==1||!s.empty())
+    {
+        cout<<"Imbalanced\n";
+    }
+    else
+    {
+        cout<<"Balanced\n";
+    }
+    
+}
